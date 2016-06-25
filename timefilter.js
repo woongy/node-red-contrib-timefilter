71c45e9097e5746f2f373db64d82903ccebbd6a5
Woongcheol Yang <woongcheol@gmail.com>
2016-06-25 21:03:55 +0900

    Send an empty message to the second output if received no inputs

--- timefilter.js
+++ timefilter.js
@@ -1,5 +1,6 @@
 module.exports = function(RED) {
   "use strict";
+  var CronJob = require("cron").CronJob;
 
   function TimefilterNode(config) {
     RED.nodes.createNode(this, config);
@@ -12,8 +13,33 @@ module.exports = function(RED) {
     this.thu = config.thu;
     this.fri = config.fri;
     this.sat = config.sat;
+    this.cronjob = null;
+    this.called = 0;
 
     var node = this;
+
+    var endhour = Math.floor(node.endtime / 60);
+    var endmin = node.endtime % 60;
+    var days = [];
+    if (node.sun) { days.push(0); }
+    if (node.mon) { days.push(1); }
+    if (node.tue) { days.push(2); }
+    if (node.wed) { days.push(3); }
+    if (node.thu) { days.push(4); }
+    if (node.fri) { days.push(5); }
+    if (node.sat) { days.push(6); }
+    days = days.join();
+
+    node.cronjob = new CronJob({
+      cronTime: endmin + " " + endhour + " * * " + days,
+      onTick: function() {
+        if (node.called == 0) {
+          node.send([null, {}]);
+        }
+        node.called = 0;
+      },
+      start: true
+    });
 
     node.on("input", function(msg) {
       var now = new Date();
@@ -27,21 +53,31 @@ module.exports = function(RED) {
       if ((node.starttime <= today) && (today <= node.endtime)) {
         switch (now.getDay()) {
           case 0:
-            if (node.sun) { node.send(msg); } break;
+            if (node.sun) { onCall(msg); } break;
           case 1:
-            if (node.mon) { node.send(msg); } break;
+            if (node.mon) { onCall(msg); } break;
           case 2:
-            if (node.tue) { node.send(msg); } break;
+            if (node.tue) { onCall(msg); } break;
           case 3:
-            if (node.wed) { node.send(msg); } break;
+            if (node.wed) { onCall(msg); } break;
           case 4:
-            if (node.thu) { node.send(msg); } break;
+            if (node.thu) { onCall(msg); } break;
           case 5:
-            if (node.fri) { node.send(msg); } break;
+            if (node.fri) { onCall(msg); } break;
           case 6:
-            if (node.sat) { node.send(msg); } break;
+            if (node.sat) { onCall(msg); } break;
         }
       }
+
+      function onCall(msg) {
+        node.called += 1;
+        node.send([msg, null]);
+      }
+    });
+
+    node.on("close", function() {
+      node.cronjob.stop();
+      delete node.cronjob;
     });
   }
 
